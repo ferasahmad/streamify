6564a89f31f95bcae1f754f9092179d573e6c54e
Feras Ahmad <firastaf878@gmail.com>
2024-08-30 15:08:23 -0400

    new: user growth chart

---
+++ src/components/sections/UserGrowthChart.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React from "react";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { UserGrowth } from "@/types";
+
+interface UserGrowthChartProps {
+  userGrowth: UserGrowth;
+}
+
+const UserGrowthChart: React.FC<UserGrowthChartProps> = ({ userGrowth }) => {
+  const chartData = userGrowth.months.map((month, index) => ({
+    month,
+    totalUsers: userGrowth.totalUsers[index],
+    activeUsers: userGrowth.activeUsers[index],
+  }));
+
+  const chartConfig: ChartConfig = {
+    totalUsers: {
+      label: "Total Users",
+      color: "hsl(var(--chart-1))",
+    },
+    activeUsers: {
+      label: "Active Users",
+      color: "hsl(var(--chart-2))",
+    },
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>User Growth</CardTitle>
+        <CardDescription>User growth trends this year.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <YAxis />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="totalUsers"
+              type="natural"
+              stroke={chartConfig.totalUsers.color}
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="activeUsers"
+              type="natural"
+              stroke={chartConfig.activeUsers.color}
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default UserGrowthChart;
